2262474270c0d641ac239b20881f19f1182e6839
ErnestTester <profile.tester1@outlook.com>
2024-07-21 18:29:03 -0600

    test

---
+++ CSharpWithSelenium/CSharpWithSelenium/FirstTestCase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace CSharpWithSelenium
+{
+    internal class FirstTestCase
+    {
+        static void Main(string[] args)
+        {
+
+            //This Example just open a Website on Chrome
+            //Ernesto Gonzalez Olaez
+            IWebDriver driver=new ChromeDriver();
+            driver.Url="https://demoqa.com/";
+            
+        }
+    }
+}
